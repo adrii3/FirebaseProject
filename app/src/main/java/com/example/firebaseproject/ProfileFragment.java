18cb9072ac870ec5b6b8d060db67d4ecaa57ef7f
dam2a <aquinoa@elpuig.xeill.net>
2020-02-17 19:52:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/firebaseproject/ProfileFragment.java
@@ -0,0 +1,55 @@
+package com.example.firebaseproject;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileFragment extends Fragment {
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+
+    public class XXXXXXFragment extends Fragment {
+
+        NavController navController;   // <-----------------
+
+        public XXXXXXFragment() {}
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_profile, container, false);
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+
+            navController = Navigation.findNavController(view);  // <-----------------
+        }
+    }
+
+}
